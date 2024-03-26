ad66a643463f9f12630c1c6c2f986fd8bb1cfa24
moraadiana <97588455+moraadiana@users.noreply.github.com>
2024-03-26 14:43:30 +0300

    done

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -50,7 +50,7 @@ def python(text="is cool"):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if n is an integer."""
+    """Display 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
 
