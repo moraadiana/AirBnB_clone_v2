edc44046b46740aacefde035b0eab4475bf2d250
moraadiana <97588455+moraadiana@users.noreply.github.com>
2024-03-26 14:48:48 +0300

    done

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Display an HTML page with a list of all states and related cities.
+    """Displays an HTML page with a list of all states and related cities.
 
     States/cities are sorted by name.
     """
